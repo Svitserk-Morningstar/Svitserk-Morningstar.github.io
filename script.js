2d5e067c73d042811778155efc73c9870554753b
Svitserk-Morningstar <133175423+Svitserk-Morningstar@users.noreply.github.com>
2023-05-25 02:21:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,73 @@
-const tab = (specifyTab) => {
-	$(".section-container").hide();
-	let cityElement = $("#" + specifyTab);
-	if (cityElement.length > 0) {
-		cityElement.css({ display: "flex", opacity: "0" });
-		setTimeout(() => {
-			cityElement.css({ transition: "opacity 0.5s ease-in-out", opacity: "1" });
-		}, 0);
+function ping(url) {
+	let startTime = new Date().getTime();
+	let xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+			let endTime = new Date().getTime();
+			let pingTime = endTime - startTime;
+			document.getElementById("pingCounter").innerHTML = pingTime;
+		}
+	};
+
+	xhr.open("GET", url, true);
+	xhr.send();
+}
+
+function startPinging(url) {
+	setInterval(function () {
+		ping(url);
+	}, 1000);
+}
+
+startPinging("https://svitserk-morningstar.github.io");
+
+let fps = 0;
+let frames = 0;
+
+function updateFPS() {
+	fps = frames;
+	frames = 0;
+	document.getElementById("fps-counter").innerText = fps;
+}
+
+function countFrames() {
+	frames++;
+}
+
+setInterval(updateFPS, 1000);
+requestAnimationFrame(function loop() {
+	countFrames();
+	requestAnimationFrame(loop);
+});
+
+function incrementCounter() {
+	if (localStorage.getItem("count")) {
+		let count = parseInt(localStorage.getItem("count"));
+		count++;
+		localStorage.setItem("count", count);
+	} else {
+		localStorage.setItem("count", 1);
 	}
-};
+	document.getElementById("counter").innerHTML = localStorage.getItem("count");
+}
+
+function changeTab(evt, name) {
+	let i, sectionContainer, navButtons;
+	sectionContainer = document.getElementsByClassName("section-container");
+	for (i = 0; i < sectionContainer.length; i++) {
+		sectionContainer[i].style.display = "none";
+	}
+	navButtons = document.getElementsByClassName("nav-buttons");
+	for (i = 0; i < navButtons.length; i++) {
+		navButtons[i].classList.remove("active");
+		navButtons[i].classList.add("inactive");
+	}
+	document.getElementById(name).style.display = "flex";
+	evt.currentTarget.classList.remove("inactive");
+	evt.currentTarget.classList.add("active");
+}
+
 function createNotification(text) {
 	const container = $("#notificationContainer");
 	const notification = $("<div>")
@@ -27,57 +87,20 @@ function createNotification(text) {
 	document.execCommand("copy");
 	el.remove();
 }
-function updateClock() {
-	const now = new Date();
-	let hours = now.getHours();
-	const minutes = ("0" + now.getMinutes()).slice(-2);
-	const seconds = ("0" + now.getSeconds()).slice(-2);
-	const milliseconds = ("00" + now.getMilliseconds()).slice(-3);
-	const meridiem = hours >= 12 ? "PM" : "AM";
-	hours = (hours % 12 || 12).toString().padStart(2, "0");
-	const timeString = hours + ":" + minutes + ":" + seconds + "." + milliseconds + " " + meridiem;
-	const clockElement = $(".clock");
-	clockElement.text(timeString);
-	requestAnimationFrame(updateClock);
-}
-requestAnimationFrame(updateClock);
-let frames = 0;
-let startTime = performance.now();
-function updateFPSCounter() {
-	frames++;
-	const currentTime = performance.now();
-	const elapsedSeconds = (currentTime - startTime) / 1000;
-	if (elapsedSeconds >= 1) {
-		const fps = Math.round(frames / elapsedSeconds);
-		$("#fps-value").text(fps);
-		frames = 0;
-		startTime = currentTime;
+
+let selectElement = document.getElementById("Urban-Dubov-sploop-scripts");
+let urbanContainer = document.getElementById("urbans-container");
+let om07Container = document.getElementById("Om07s-container");
+
+selectElement.addEventListener("change", function () {
+	switch (selectElement.value) {
+		case "choose-sploop-Om07":
+			urbanContainer.style.display = "none";
+			om07Container.style.display = "grid";
+			break;
+		case "choose-sploop-Urban-Dubov":
+			urbanContainer.style.display = "grid";
+			om07Container.style.display = "none";
+			break;
 	}
-	requestAnimationFrame(updateFPSCounter);
-}
-updateFPSCounter();
-function measurePing() {
-	let startTime = new Date().getTime();
-	let xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4) {
-			let endTime = new Date().getTime();
-			let pingTime = endTime - startTime;
-			document.getElementById("ping-counter").textContent = "Ping: " + pingTime + " ms";
-		}
-	};
-	xhr.open("GET", "https://svitserk-morningstar.github.io", true);
-	xhr.send();
-}
-setInterval(measurePing, 1000);
-if (typeof Storage !== "undefined") {
-	localStorage.setItem.visitcount;
-	if (localStorage.visitcount) {
-		document.getElementById("count").innerHTML = localStorage.visitcount;
-		localStorage.visitcount = Number(localStorage.visitcount) + 1;
-	} else {
-		localStorage.visitcount = 1;
-	}
-} else {
-	document.getElementById("count").innerHTML = "error";
-}
+});
