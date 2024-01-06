c70aba66ab0f32a2c726cc0384a570af253fdb5b
Svitserk-Morningstar <133175423+Svitserk-Morningstar@users.noreply.github.com>
2024-01-07 01:39:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function changeTab(evt, cityName) {
-  var i, section, button;
-  section = document.getElementsByClassName("section📁");
-  for (i = 0; i < section.length; i++) {
-    section[i].style.display = "none";
-  }
-  button = document.getElementsByClassName("button📁");
-  for (i = 0; i < button.length; i++) {
-    button[i].className = button[i].className.replace(" active📁", "");
-  }
-  document.getElementById(cityName).style.display = "flex";
-  evt.currentTarget.className += " active📁";
+  $(".section📁").hide();
+  $(".button📁").removeClass("active📁");
+  $("#" + cityName).css("display", "flex");
+  $(evt.currentTarget).addClass("active📁");
 }
